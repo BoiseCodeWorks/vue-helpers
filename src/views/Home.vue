f6382e9f6e87145fde0acf43d1e9624e915c73d0
Jake Overall <joverall22@gmail.com>
2019-09-12 18:07:42 -0600

    add simple modal

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+
+    <button @click="showformModal = true">trigger</button>
+
+    <quick-modal class="bg-dark text-light" :toggle="showformModal" @close="showformModal = false">
+      <form @submit.prevent="submit">
+        <div class="form-group">
+          <label for>Something Here</label>
+          <input type="text" />
+        </div>
+      </form>
+
+      <div slot="modal-footer">
+        <button type="button" @click="submit" class="btn btn-success">Submit</button>
+      </div>
+    </quick-modal>
+
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
+  name: "home",
+  data() {
+    return {
+      showformModal: false
+    };
+  },
   components: {
     HelloWorld
+  },
+  methods:{
+    submit(){
+      console.log("cool")
+      this.showformModal = false;
+    }
   }
-}
+};
 </script>
